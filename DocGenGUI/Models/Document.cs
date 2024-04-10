dea461b11ffb838aacdd29945206f0f343382111
Lesedi Jonas <lesedij@bbd.co.za>
2024-04-10 21:46:05 +0200

    intergration

---
+++ DocGenGUI/Models/Document.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocGen.Models
+{
+    internal class Document
+    {
+        public string fileName { get; set; }
+        public string content { get; set; }
+
+        public Document() 
+        { 
+            fileName = string.Empty; 
+            content = string.Empty;
+        }
+
+        public Document(string fileName, string content)
+        {
+            this.fileName = fileName;
+            this.content = content;
+        }
+    }
+}
--- DocGenGUI/Models/FileModel.cs
+++ DocGenGUI/Models/FileModel.cs
@@ -8,9 +8,25 @@ namespace DocGen.Models
 {
     internal class FileModel
     {
-        public string? FileName { get; set; }
-        public string? FileContents { get; set; }
-        public string? FilePath { get; set; }
-        public string? FileSummary { get; set; }
+        public string FileName { get; set; }
+        public string FileContents { get; set; }
+        public string FilePath { get; set; }
+        public string FileSummary { get; set; }
+
+        public FileModel() 
+        { 
+            this.FileName = string.Empty;
+            this.FileContents = string.Empty;
+            this.FileSummary = string.Empty;
+            this.FileSummary = string.Empty;
+        }
+
+        public FileModel(string fileName, string fileContents, string filePath, string fileSummary)
+        {
+            FileName = fileName;
+            FileContents = fileContents;
+            FilePath = filePath;
+            FileSummary = fileSummary;
+        }
     }
 }

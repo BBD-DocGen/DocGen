7b10cba78940ce2a01e6714be6a888753919e2cd
devonY3303 <159463977+devonY3303@users.noreply.github.com>
2024-04-12 08:56:06 +0200

    Cli clean up (#25)
    * Clean up, improve the welcome text, error messages for exceptional conditions
    
    * Refactored main, and removed unnecessary library usage
    
    * Added cli executable workflow
    
    * update workflow
    
    ---------
    
    Co-authored-by: Tshego-MM <tshegofatso.mohlala@bbd.co.za>

--- DocGen/LogIn/LogIn.cs
+++ DocGen/LogIn/LogIn.cs
@@ -4,122 +4,126 @@ using Newtonsoft.Json;
 using System.Net;
 using DocGen.Classes;
 
-class LogIn{
-    public static string Login()
-    {
-      try
+namespace CliProgram
+{
+    class LogIn{
+        public static string Login()
         {
-            var response = requestDeviceCodeUri();
-
-            if (response != null)
+            try
             {
-                if (response.StatusCode != HttpStatusCode.OK){
-                  InterfaceText.insertBoundaryText();
-                  Console.WriteLine("Cannot make request to the Autho website, please check your network connection and try again.");
-                  return "";
-                }
-                else{
-                  var responseObject = JsonConvert.DeserializeObject<dynamic>(response.Content);
-                  string verification_uri_complete = responseObject.verification_uri_complete;
-                  openUri(verification_uri_complete);
+                var response = requestDeviceCodeUri();
 
-                  Console.WriteLine("Verification page has been opened on your default browser, or go to below link:");
-                  Console.WriteLine($"{responseObject.verification_uri_complete}");
-                  Console.WriteLine($"Your One-time Code is: {responseObject.user_code}, please use this to link your device\n");
-                  Console.WriteLine();
-                  Console.WriteLine("1. Continue if you are successfully connected");
-                  Console.WriteLine("2. Exit log in");
-                  var oAuthOption = Console.ReadLine();
-                  Console.WriteLine();
-
-                  while (oAuthOption != "1"){
-                    if (oAuthOption == "2"){
-                      InterfaceText.insertBoundaryText();
-                      Console.WriteLine("You have exited the log in process.");
-                      return "";
+                if (response != null)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK){
+                        InterfaceText.insertBoundaryText();
+                        Console.WriteLine("Cannot make request to the Autho website, please check your network connection and try again.");
+                        return "";
                     }
-                    Console.WriteLine("Verification page has been opened on your default browser, or go to below link:");
-                    Console.WriteLine($"{responseObject.verification_uri_complete}");
-                    Console.WriteLine($"Your One-time Code is: {responseObject.user_code}");
-                    Console.WriteLine("1. Continue if you are successfully connected");
-                    Console.WriteLine("2. Exit log in");
-                    oAuthOption = Console.ReadLine();
-                  }
+                    else{
+                        var responseObject = JsonConvert.DeserializeObject<dynamic>(response.Content);
+                        string verification_uri_complete = responseObject.verification_uri_complete;
+                        openUri(verification_uri_complete);
 
-                  response = requestTokenUri(responseObject);
-                  if (response.StatusCode != HttpStatusCode.OK){
-                    InterfaceText.insertBoundaryText();
-                    Console.WriteLine("Your device is not succesfully connected, please try again.");
+                        Console.WriteLine("Verification page has been opened on your default browser, or go to below link:");
+                        Console.WriteLine($"{responseObject.verification_uri_complete}");
+                        Console.WriteLine($"Your One-time Code is: {responseObject.user_code}, please use this to link your device\n");
+                        Console.WriteLine();
+                        Console.WriteLine("1. Continue if you are successfully connected");
+                        Console.WriteLine("2. Exit log in");
+                        var oAuthOption = Console.ReadLine();
+                        Console.WriteLine();
+
+                        while (oAuthOption != "1"){
+                            if (oAuthOption == "2"){
+                                InterfaceText.insertBoundaryText();
+                                Console.WriteLine("You have exited the log in process.");
+                                return "";
+                            }
+                            InterfaceText.insertBoundaryText();
+                            Console.WriteLine("Verification page has been opened on your default browser, or go to below link:");
+                            Console.WriteLine($"{responseObject.verification_uri_complete}");
+                            Console.WriteLine($"Your One-time Code is: {responseObject.user_code}, please use this to link your device\n");
+                            Console.WriteLine("1. Continue if you are successfully connected");
+                            Console.WriteLine("2. Exit log in");
+                            oAuthOption = Console.ReadLine();
+                        }
+
+                        response = requestTokenUri(responseObject);
+                        if (response.StatusCode != HttpStatusCode.OK){
+                            InterfaceText.insertBoundaryText();
+                            Console.WriteLine("Your device is not succesfully connected yet, please try again.");
+                            return "";
+                        }
+                        else{
+                            responseObject = JsonConvert.DeserializeObject<dynamic>(response.Content);
+                            var access_token = responseObject.access_token;
+                            InterfaceText.insertBoundaryText();
+                            return access_token;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No response received.");
                     return "";
-                  }
-                  else{
-                    responseObject = JsonConvert.DeserializeObject<dynamic>(response.Content);
-                    var access_token = responseObject.access_token;
-                    InterfaceText.insertBoundaryText();
-                    return access_token;
-                  }
                 }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("No response received.");
+                Console.WriteLine($"Error: {ex.Message}");
                 return "";
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-            return "";
+
+        public static RestResponse requestDeviceCodeUri(){
+            var client = new RestClient("https://dev-2f8sdpf6pls655l7.us.auth0.com/oauth/device/code");
+            var request = new RestRequest();
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", "client_id=RvnPGiHVhtjPJ76vzfaIM0WmidvjRwl7&scope=openid%20profile%20email&audience=https://docgen.com", ParameterType.RequestBody);
+            RestResponse response = null;
+
+            try{
+              response = client.ExecutePost(request);
+            }
+            catch (Exception ex){
+                  Console.WriteLine($"Error: {ex.Message}");
+            }
+            return response;
         }
-    }
 
-    public static RestResponse requestDeviceCodeUri(){
-      var client = new RestClient("https://dev-2f8sdpf6pls655l7.us.auth0.com/oauth/device/code");
-      var request = new RestRequest();
-      request.AddHeader("content-type", "application/x-www-form-urlencoded");
-      request.AddParameter("application/x-www-form-urlencoded", "client_id=RvnPGiHVhtjPJ76vzfaIM0WmidvjRwl7&scope=openid%20profile%20email&audience=https://docgen.com", ParameterType.RequestBody);
-      RestResponse response = null;
+        public static RestResponse requestTokenUri(dynamic responseObject){
+            var client = new RestClient("https://dev-2f8sdpf6pls655l7.us.auth0.com/oauth/token");
+            var request = new RestRequest();
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", $"grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code&device_code={responseObject.device_code}&client_id=RvnPGiHVhtjPJ76vzfaIM0WmidvjRwl7", ParameterType.RequestBody);
+            RestResponse response = null;
 
-      try{
-        response = client.ExecutePost(request);
-      }
-      catch (Exception ex){
-            Console.WriteLine($"Error: {ex.Message}");
-      }
-      return response;
-    }
+            try{
+              response = client.ExecutePost(request);
+            }
+            catch (Exception ex){
+                  Console.WriteLine($"Error: {ex.Message}");
+            }
+            return response;
+        }
 
-    public static RestResponse requestTokenUri(dynamic responseObject){
-      var client = new RestClient("https://dev-2f8sdpf6pls655l7.us.auth0.com/oauth/token");
-      var request = new RestRequest();
-      request.AddHeader("content-type", "application/x-www-form-urlencoded");
-      request.AddParameter("application/x-www-form-urlencoded", $"grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code&device_code={responseObject.device_code}&client_id=RvnPGiHVhtjPJ76vzfaIM0WmidvjRwl7", ParameterType.RequestBody);
-      RestResponse response = null;
+        public static void openUri(string verification_uri_complete){
+            string auth0VerificationUri = verification_uri_complete;
+            var auth0VerificationUriInfo = new ProcessStartInfo {
+                FileName = auth0VerificationUri,
+                UseShellExecute = true
+            };
+            Process.Start(auth0VerificationUriInfo);
+        }
 
-      try{
-        response = client.ExecutePost(request);
-      }
-      catch (Exception ex){
-            Console.WriteLine($"Error: {ex.Message}");
-      }
-      return response;
-    }
-
-    public static void openUri(string verification_uri_complete){
-      string auth0VerificationUri = verification_uri_complete;
-      var auth0VerificationUriInfo = new ProcessStartInfo {
-          FileName = auth0VerificationUri,
-          UseShellExecute = true
-      };
-      Process.Start(auth0VerificationUriInfo);
-    }
-
-    public static async Task<bool> LogInUserCheck(string access_token){
-      Provider.setHeader(access_token);
-      var loginCheck = await Provider.login();
-      if (loginCheck){
-        return true;
-      }
-      return false;
+        public static async Task<bool> LogInUserCheck(string access_token){
+            Provider.setHeader(access_token);
+            var loginCheck = await Provider.login();
+            if (loginCheck){
+              return true;
+            }
+            return false;
+        }
     }
 }
